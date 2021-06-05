f4c14f6d555bf02327d86bbd11490154b5488139
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-06-05 15:30:05 -0400

    Completed basic HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export function App() {
-  return <div>Hello, World!</div>
+  const [weather, setWeather] = useState({})
+  return (
+    <main>
+      <h1>Lamest Conversation Topic</h1>
+      <div>
+        <ol>
+          <ul>
+            <li>
+              <form>
+                <input type="text" placeholder="Zip Code" />
+              </form>
+            </li>
+            <li>
+              <button>Check</button>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <form>
+                <input type="text" placeholder="City" />
+              </form>
+            </li>
+            <li>
+              <button>Check</button>
+            </li>
+          </ul>
+        </ol>
+        <ol>
+          <li>Conditions:</li>
+          <li>Cloud Coverage:</li>
+          <li>Temp:</li>
+          <li>Humidity:</li>
+          <li>Feels Like:</li>
+          <li>Wind: Speed Direction</li>
+        </ol>
+      </div>
+    </main>
+  )
 }
