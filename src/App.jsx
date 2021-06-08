8784f55d75a98a7be5d67e8a4449b056c7dfe148
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-06-08 14:10:42 -0400

    adjusted app

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export function App() {
-  const [weatherInfo, setWeatherInfo] = useState({ weatherInfo: [] })
+  const [weatherInfo, setWeatherInfo] = useState({})
 
   const [zipCode, setZipCode] = useState(10000)
   const [city, setCity] = useState('City')
 
   function updateZipCode(event) {
     setZipCode(event.target.value)
+    console.log('zipcode')
   }
 
   function updateCity(event) {
     setCity(event.target.value)
+    console.log('city')
   }
 
   async function zipCodeWeather() {
@@ -21,6 +23,7 @@ export function App() {
         `api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=038ca81c8c47ce0022176b5cdad909de&units=imperial`
       )
       .then((resp) => setWeatherInfo(resp.data))
+    console.log(weatherData)
   }
 
   async function cityWeather() {
@@ -29,15 +32,16 @@ export function App() {
         `api.openweathermap.org/data/2.5/weather?q=${city}&appid=038ca81c8c47ce0022176b5cdad909de&units=imperial`
       )
       .then((resp) => setWeatherInfo(resp.data))
+    console.log(weatherData)
   }
 
   useEffect(() => {
     zipCodeWeather()
-  }, [])
+  }, [zipCode])
 
   useEffect(() => {
     cityWeather()
-  }, [])
+  }, [city])
 
   //`api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=038ca81c8c47ce0022176b5cdad909de&units=imperial`
 
@@ -57,7 +61,7 @@ export function App() {
               </form>
             </li>
             <li>
-              <button onClick={this.zipCodeWeather}>Check</button>
+              <button onClick={zipCodeWeather}>Check</button>
             </li>
           </ul>
           <ul>
@@ -67,18 +71,22 @@ export function App() {
               </form>
             </li>
             <li>
-              <button onClick={this.cityWeather}>Check</button>
+              <button onClick={cityWeather}>Check</button>
             </li>
           </ul>
         </ol>
-        <ol>
-          <li>Conditions: {weatherInfo.weather.description} </li>
-          <li>Cloud Coverage: {weatherInfo.clouds.all} % </li>
-          <li>Temp: {weatherInfo.main.temp} F </li>
-          <li>Humidity: {weatherInfo.main.humidity} %</li>
-          <li>Feels Like: {weatherInfo.main.feels_like} F</li>
-          <li>Wind: {weatherInfo.wind.speed} MPH</li>
-        </ol>
+        {weatherInfo.weather ? (
+          <ol>
+            <li>Conditions: {weatherInfo.weather[0].description} </li>
+            <li>Cloud Coverage: {weatherInfo.clouds.all} % </li>
+            <li>Temp: {weatherInfo.main.temp} F </li>
+            <li>Humidity: {weatherInfo.main.humidity} %</li>
+            <li>Feels Like: {weatherInfo.main.feels_like} F</li>
+            <li>Wind: {weatherInfo.wind.speed} MPH</li>
+          </ol>
+        ) : (
+          <li>loading</li>
+        )}
       </div>
     </main>
   )
